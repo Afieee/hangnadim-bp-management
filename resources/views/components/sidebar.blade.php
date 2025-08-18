33abbc62d8db990ba242e07bb8557f2839ed8e31
Afieee <afiwijaya555@gmail.com>
2025-08-18 18:24:50 +0700

    Adding multiple role authtentication

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,28 +1,25 @@
     <!-- Sidebar -->
-    <div class="sidebar" id="sidebar">
-        <div class="sidebar-header">
-            <h3>                    
-                <img src="{{ asset('/storage/images/logo_bp.png') }}" alt="User Profile" style="width: 100px; height: 100px;">
-                BP Batam
-            </h3>
-            <div class="sidebar-close" id="sidebar-close">
-                <i class="fas fa-times"></i>
+        <div class="sidebar" id="sidebar">
+            <div class="sidebar-header">
+                <h3>                    
+                    <img src="{{ asset('/storage/images/logo_bp.png') }}" alt="User Profile" style="width: 100px; height: 100px;">
+                    BP Batam
+                </h3>
+                <div class="sidebar-close" id="sidebar-close">
+                    <i class="fas fa-times"></i>
+                </div>
             </div>
+            <ul class="sidebar-menu">
+                @if (Auth::user()->role == "admin")
+                    <li class="active"><a href="/dashboard"><i class="fas fa-home"></i> Home</a></li>
+                    <li><a href="/jadwalkan-inspeksi"><i class="fas fa-calendar-alt"></i> Jadwalkan Inspeksi</a></li>
+                    <li><a href="/halaman-registrasi"><i class="fas fa-users"></i> Pengguna & Petugas</a></li>
+                @elseif (Auth::user()->role == "petugas")
+                    <li class="active"><a href="/dashboard"><i class="fas fa-home"></i> Home</a></li>
+                    <li><a href="#"><i class="fas fa-clipboard-check"></i> Proses Inspeksi</a></li>
+                @endif
+
+            </ul>
         </div>
-        <ul class="sidebar-menu">
-            <li class="active"><a href="/dashboard"><i class="fas fa-home"></i> Home</a></li>
-            {{-- <li><a href="#"><i class="fas fa-chart-network"></i> Analytics</a></li>
-            <li><a href="#"><i class="fas fa-shopping-bag"></i> Orders</a></li>
-            <li><a href="#"><i class="fas fa-cube"></i> Products</a></li> --}}
-            {{-- 
-            <li><a href="#"><i class="fas fa-cog"></i> Settings</a></li>
-            <li><a href="#"><i class="fas fa-question-circle"></i> Help Center</a></li>
-            <li><a href="#"><i class="fas fa-envelope"></i> Messages <span class="badge">5</span></a></li>
-            <li><a href="#"><i class="fas fa-file-chart-line"></i> Reports</a></li> --}}
 
-            <li><a href="/jadwalkan-inspeksi"><i class="fas fa-calendar-alt"></i> Jadwalkan Inspeksi</a></li>
-            <li><a href="/halaman-registrasi"><i class="fas fa-users"></i> Pengguna & Petugas</a></li>
-
-
-        </ul>
-    </div>
+    
